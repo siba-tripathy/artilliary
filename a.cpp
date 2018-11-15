ab1595ff5d93468bd2e42f29357a6797650eb76c
siba-tripathy <sibatripathy19@gmail.com>
2018-11-15 08:58:54 +0530

    startup done

--- a.cpp
+++ a.cpp
@@ -1,7 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
+void startup()
+{
+	cout<<"\t\tWelcome to Artillary Station.\t\n";
+	cout<<"You are facing a fierce enemy and you have got 10 cannon balls to fire.\n";
+	cout<<"Try to hit the enemy within ten hits, or you'll die\n";
+}
+void Fire()
 int main()
 {
+	startup();
 	int t;
 	cin>>t;
 	while(t--)
